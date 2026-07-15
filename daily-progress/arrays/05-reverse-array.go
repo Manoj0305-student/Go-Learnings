ead7561318694fc7aca2173a22b02ac0a837bcef
Manoj0305-student <128007896+Manoj0305-student@users.noreply.github.com>
2026-07-15 15:54:38 +0530

    Refactor comments in ReverseArray function for clarity and readability

--- daily-progress/arrays/05-reverse-array.go
+++ daily-progress/arrays/05-reverse-array.go
@@ -2,9 +2,11 @@ package arrays
 import "fmt"
 
 /* 
-	Any changes you make to arr inside ReverseArray are only happening to that local copy.    The original array you passed in from main() will remain completely unchanged.
+	Any changes you make to arr inside ReverseArray are only happening to that local copy.    
+	The original array you passed in from main() will remain completely unchanged.
 
-	To fix this, you should use a slice ([]int) instead of a fixed-size array ([5]int). Slices in Go act as references to the underlying array, meaning changes made inside the function will reflect on the original data.
+	To fix this, you should use a slice ([]int) instead of a fixed-size array ([5]int). 
+	Slices in Go act as references to the underlying array, meaning changes made inside the function will reflect on the original data.
 
 	In coming problems we will use slice concept we will cover this topic next lessons with 
 	detailed explaination.
